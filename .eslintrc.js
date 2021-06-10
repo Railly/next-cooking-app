81d86969befa1684cf454e7f0984d2a1d3a32648
Railly Hugo <railly_2009@hotmail.com>
2021-06-09 23:53:20 -0500

    FEAT: Add nav layout

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:react/recommended", "standard"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "no-irregular-whitespace": "off",
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,62 @@
+import Logo from 'components/Logo'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp ({ Component, pageProps }) {
+  const { pathname } = useRouter()
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Logo />
+          </li>
+          <div>
+            <li>
+              <Link href="/">
+                <a className={pathname === '/' ? 'current_page' : ''}>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/services">
+                <a className={pathname === '/services' ? 'current_page' : ''}>
+                  Servicios
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/pricing">
+                <a className={pathname === '/pricing' ? 'current_page' : ''}>
+                  Precios
+                </a>
+              </Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
+      <Component {...pageProps} />
+      <style jsx>{`
+        li {
+          list-style: none;
+        }
+
+        ul {
+          display: flex;
+        }
+
+        div {
+          display: flex;
+          justify-content: space-evenly;
+          width: 50%;
+        }
+
+        .current_page {
+          color: #ff6961;
+        }
+      `}</style>
+    </>
+  )
 }
 
 export default MyApp
