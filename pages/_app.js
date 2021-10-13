c308b3987d55c1ed9f80ffc281fa58aabde78391
Railly <railly_2009@hotmail.com>
2021-10-13 15:22:47 -0500

    Testing GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,50 @@
-import AuthLayout from 'components/Layouts/AuthLayout'
-import BrowseLayout from 'components/Layouts/BrowseLayout'
-import LandingLayout from 'components/Layouts/LandingLayout'
-import { listenLatestCookbooks } from 'firebase/client'
-import useUser from 'hooks/useUser'
-import Head from 'next/head'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import { AUTH_PAGES, BROWSE_PAGES, LANDING_PAGES, re } from 'utils/dictionary'
-import '../styles/globals.css'
+import AuthLayout from "components/Layouts/AuthLayout";
+import BrowseLayout from "components/Layouts/BrowseLayout";
+import LandingLayout from "components/Layouts/LandingLayout";
+import { listenLatestCookbooks } from "firebase/client";
+import useUser from "hooks/useUser";
+import { GA_TRACKING_ID, pageview } from "lib/gtag";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { AUTH_PAGES, BROWSE_PAGES, LANDING_PAGES, re } from "utils/dictionary";
+import "../styles/globals.css";
 
-function MyApp ({ Component, pageProps }) {
-  const user = useUser()
-  const [cookbooks, setCookbooks] = useState([])
-  const { pathname } = useRouter()
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const user = useUser();
+  const [cookbooks, setCookbooks] = useState([]);
+  const { pathname } = useRouter();
 
   useEffect(() => {
-    let unsubscribe
+    const handleRouteChange = (url) => {
+      pageview(url);
+      console.log(GA_TRACKING_ID);
+      console.log(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  useEffect(() => {
+    let unsubscribe;
     if (user) {
-      unsubscribe = listenLatestCookbooks(user.uid, setCookbooks)
+      unsubscribe = listenLatestCookbooks(user.uid, setCookbooks);
     }
-    return () => unsubscribe && unsubscribe()
-  }, [user])
+    return () => unsubscribe && unsubscribe();
+  }, [user]);
 
   return (
     <>
       <Head>
         <title>
-          Foody -{' '}
+          Foody -{" "}
           {LANDING_PAGES[pathname] ||
             AUTH_PAGES[pathname] ||
             BROWSE_PAGES[pathname] ||
-            (re.test(pathname) && 'Crea')}
+            (re.test(pathname) && "Crea")}
         </title>
         <meta
           name="description"
@@ -50,13 +64,13 @@ function MyApp ({ Component, pageProps }) {
         </AuthLayout>
       )}
       {re.test(pathname) && (
-        <div style={{ display: 'grid', gridTemplateColumns: '2fr 25fr 45fr' }}>
+        <div style={{ display: "grid", gridTemplateColumns: "2fr 25fr 45fr" }}>
           <BrowseLayout />
           <Component user={user} cookbooks={cookbooks} {...pageProps} />
         </div>
       )}
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
